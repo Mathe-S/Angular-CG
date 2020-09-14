d963dbf66a081dabe6240219d9ba9b9db79e571e
Mathe Sharvadze <sharvadzemat@gmail.com>
2020-09-14 19:01:46 +0400

    added collections module

--- comps/src/app/collections/collections-home/collections-home.component.ts
+++ comps/src/app/collections/collections-home/collections-home.component.ts
@@ -3,13 +3,23 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-collections-home',
   templateUrl: './collections-home.component.html',
-  styleUrls: ['./collections-home.component.css']
+  styleUrls: ['./collections-home.component.css'],
 })
 export class CollectionsHomeComponent implements OnInit {
+  data = [
+    { name: 'John', age: 21, job: 'Designer', employed: false },
+    { name: 'David', age: 26, job: 'Engineer', employed: true },
+    { name: 'Hoe', age: 19, job: 'Hoe', employed: false },
+  ];
 
-  constructor() { }
+  headers = [
+    { key: 'name', label: 'name' },
+    { key: 'age', label: 'age' },
+    { key: 'job', label: 'job' },
+    { key: 'employed', label: 'has a job?' },
+  ];
 
-  ngOnInit(): void {
-  }
+  constructor() {}
 
+  ngOnInit(): void {}
 }
